88b689e02f99cd625a5177cf72e33d9e50a6c674
Saul <saulb3564@gmail.com>
2022-10-05 23:51:36 -0500

    Se creo la estructura del componente que contendra las tareas.

---
+++ src/components/ModalContainer/ModalContainer.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Modal } from "@mui/material"
+
+import "./ModalContainer.scss"
+
+export default function ModalContainer(props) {
+    const { isOpenModal, closeModal, children } = props
+
+    return(
+        <Modal 
+        aria-labelledby = "transition modal-title"
+        aria-describedby = "transition modal-description"
+        className = "modal"
+        open = { isOpenModal }
+        onClose = { closeModal }
+        closeAfterTransition>
+            <div>
+                { children }
+            </div>
+        </Modal>
+    )
+}
