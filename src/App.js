53fcc2e4be42470455ec6e44736bb39e41935a03
Saul <saulb3564@gmail.com>
2022-10-14 20:39:36 -0500

    Se agregaron nuevos componentes ademas de agreagar la funcionalidad de poder proporcionar tareas cuando muestra el formulario.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import { Container } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Container, Snackbar} from '@mui/material';
 import Header from './components/Header/Header.js';
 import ListTareas from './components/ListTareas/ListTareas.js';
 import EnviarTarea from './components/EnviarTarea/EnviarTarea.js';
-
+import { TASKS_STORAGE } from "./utils/constants";
 
 
 function App() {
+
+  const [toastProps, setToastProps] = useState({
+    open: false,
+    text: null
+  })
+  const [allTareas, setAllTareas] = useState([])
+  const [reloadTareas, setReloadTareas] = useState(false)
+
+  useEffect(() => {
+    const allTareasStorage = localStorage.getItem(TASKS_STORAGE)
+    const allTareasArray = JSON.parse(allTareasStorage)
+
+    setAllTareas(allTareasArray)
+    setReloadTareas(false)
+  }, [reloadTareas])
+
+  const hacerTarea = index => {
+    allTareas.splice(index, 1)
+    setAllTareas(allTareas)
+    localStorage.setItem(TASKS_STORAGE, JSON.stringify(allTareas))
+    setReloadTareas(true)
+  }
+
   return (
-    <Container>
+    <Container className='lista-tareas' maxWidth={false}>
       <Header/>
-      <EnviarTarea/>
-      <ListTareas/>
+      <EnviarTarea setToastProps={setToastProps} allTareas={allTareas}/>
+      <ListTareas allTareas={allTareas} hacerTarea={hacerTarea}/>
+      <Snackbar 
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right"
+      }}
+      open={toastProps.open}
+      autoHideDuration={1000}
+      message={<span id='messege-id'>{toastProps.text}</span>}
+      />
     </Container>
   );
 }
