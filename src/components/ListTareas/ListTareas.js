e1e4e3fb30e990977e1aab04c528b7dcd431738b
Saul <saulb3564@gmail.com>
2022-10-14 20:42:02 -0500

    Se modifico el archivo.

--- src/components/ListTareas/ListTareas.js
+++ src/components/ListTareas/ListTareas.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Grid } from '@mui/material';
+import Tarea from "../Tarea/Tarea";
 
 import './ListTareas.scss';
 
 export default function ListTareas(props) {
-    const { allTareas } = props
+    const { allTareas, hacerTarea } = props
 
     if(!allTareas || (allTareas.length === 0)) {
         return (
@@ -16,7 +17,12 @@ export default function ListTareas(props) {
 
     return (
         <Grid container spacing={ 3 } className="list-tareas">
-
+            {
+            allTareas.map((tarea, index) => {
+                return (<Grid key={index} item xs={4}>
+                    <Tarea tarea={tarea} index={index} hacerTarea={hacerTarea}/>
+                </Grid>)
+            })}
         </Grid>
     );
 }
